db350180403a62061498462c2b99f8890adc3c0f
Amshu <84790256+amshuprasad@users.noreply.github.com>
2025-07-31 11:35:15 +0530

    Add files via upload

---
+++ Assignment5_Task2.py
@@ -0,0 +1,7 @@
+original_list = [1,2,3,4,5,6,7,8,9,10]
+extracted_list = original_list[:5]
+reversed_list = extracted_list[::-1]
+
+print("Original List: ",original_list)
+print("Expected List: ",extracted_list)
+print("Reversed List: ",reversed_list)
